18e5debefc048e35fd6329f3ea78168a1be0cd88
Rupin Jairaj <rupinjairaj@gmail.com>
2016-11-26 03:15:55 +0530

    Trained the linear regression model and obtained the intercept and the coefficient

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,12 +1,31 @@
 #!usr/bin/python
 
-import numpy as numpy
+import numpy as np
 import pandas as pd
-from scipy import stats
+from sklearn import linear_model
 import matplotlib.pyplot as plt
+
+# Reading data
 s1_data = pd.read_csv('Data/Mens_height_weight.csv')
+
+# Plotting the data as a scatter plot
 fig, ax = plt.subplots(1, 1)
 ax.scatter(s1_data['Height'], s1_data['Weight'])
 ax.set_xlabel('Height')
 ax.set_ylabel('Weight')
 plt.show()
+
+# To check the Pearson correlation between the 2 variables 
+print s1_data.corr()
+
+# Generating a Linear Regression model with weight as the dependent and height as independent 
+## Creating a Linear Regression model
+lm = linear_model.LinearRegression()
+
+## Training the model using the training set
+lm.fit(s1_data.Height[:, np.newaxis], s1_data.Weight)
+
+print "Intercept is {0}".format(lm.intercept_)
+print "Coefficient value of the height is {0}".format(lm.coef_)
+print pd.DataFrame(zip(s1_data.columns, lm.coef_),
+					columns = ['features', 'estimatedCoefficients'])
